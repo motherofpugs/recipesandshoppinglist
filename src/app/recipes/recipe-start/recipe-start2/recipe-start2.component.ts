23d517a3b5d4a7a3aa06d81d07deb4ce46c2a4d9
motherofpugs <csampaidominika@gmail.com>
2023-12-13 00:17:38 +0100

    start to built slider

--- src/app/recipes/recipe-start/recipe-start2/recipe-start2.component.ts
+++ src/app/recipes/recipe-start/recipe-start2/recipe-start2.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/shared/recipe.service';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-start2',
   templateUrl: './recipe-start2.component.html',
-  styleUrls: ['./recipe-start2.component.css']
+  styleUrls: ['./recipe-start2.component.css'],
 })
-export class RecipeStart2Component {
+export class RecipeStart2Component implements OnInit {
+  selectedIndex: number = 0;
+  recipes: Recipe[] = [];
 
+  constructor(private recipeService: RecipeService) {}
+  ngOnInit(): void {
+    this.recipeService.recipesChanged$.subscribe((recipes: Recipe[]) => {
+      this.recipes = recipes;
+    });
+    this.recipeService.getRecipes();
+  }
+
+  nextImg() {
+    if (this.selectedIndex >= this.recipes.length - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
+  }
 }
